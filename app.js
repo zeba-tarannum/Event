c99b113c5e82af3aae40f35f38e28f15f9103d14
unknown <tarannumzeba.tarannumzeba@gmail.com>
2020-06-04 02:02:08 -0700

    error

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static("client/build"));
+// }
+// // app.get("*", (req, res) => {
+// //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// // });
 
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 
