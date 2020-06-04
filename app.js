f9a12231c1a670708b0f9398814ed144e3523aaa
unknown <tarannumzeba.tarannumzeba@gmail.com>
2020-06-04 01:39:19 -0700

    env

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 
 app.post("/events", (req, res, next) => {
